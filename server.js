969f0e07ed6575aed77f62660b7e5403b16aa8d5
Nikola Bojovic <bojke13@gmail.com>
2018-03-19 20:57:36 +0100

    Add Project page

--- server.js
+++ server.js
@@ -74,6 +74,12 @@ app.get('/about', (req, res) => {
     })
 });
 
+app.get('/projects', (req, res) => {
+   res.render('projects.hbs', {
+       projects: 'Projects Page'
+   })
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
